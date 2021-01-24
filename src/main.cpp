0b21122ab39c8f7c492fca900acc13ce2c34a3c0
Matushkin Vladimir <matushkin.vladimir98@gmail.com>
2021-01-24 19:40:58 +0500

    Add support for ',' separator in real numbers

--- src/main.cpp
+++ src/main.cpp
@@ -10,6 +10,27 @@ namespace x3 = boost::spirit::x3;
 
 namespace calculator
 {
+    template<typename T>
+    struct real_policy : x3::real_policies<T>
+    {
+        template <typename Iterator>
+        static bool
+        parse_dot(Iterator& first, const Iterator &last)
+        {
+            if (first == last || (*first != '.' && *first != ',')) {
+                return false;
+            }
+            ++first;
+            return true;
+        }
+    };
+
+    x3::real_parser<double, real_policy<double>> real_;
+
+    const x3::rule<class expression, double> expression{"expression"};
+    const x3::rule<class term, double> term{"term"};
+    const x3::rule<class factor, double> factor{"factor"};
+
     const auto zero = [](auto &ctx){ x3::_val(ctx) = 0; };
     const auto mov = [](auto &ctx){ x3::_val(ctx) = x3::_attr(ctx); };
     const auto add = [](auto &ctx){ x3::_val(ctx) += x3::_attr(ctx); };
@@ -17,10 +38,6 @@ namespace calculator
     const auto mul = [](auto &ctx){ x3::_val(ctx) *= x3::_attr(ctx); };
     const auto div = [](auto &ctx){ x3::_val(ctx) /= x3::_attr(ctx); };
     const auto neg = [](auto &ctx){ x3::_val(ctx) = -x3::_attr(ctx); };
-
-    const x3::rule<class expression, double> expression{"expression"};
-    const x3::rule<class term, double> term{"term"};
-    const x3::rule<class factor, double> factor{"factor"};
 
     const auto expression_def =
         x3::eps[zero]
@@ -34,7 +51,7 @@ namespace calculator
             | ('/' >> factor[div])
         );
     const auto factor_def =
-        x3::double_[mov]
+        real_[mov]
         | '(' >> expression[mov] >> ')'
         | ('-' >> factor[neg])
         | ('+' >> factor[mov])
